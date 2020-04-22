37da382432b19b8897972f82f51abdab7e15a626
SeonJin jang <seonjin85@gmail.com>
2020-04-22 16:13:24 +0900

    installing express with babel, parser, helmet, morgan

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("gi");
+//const express = require('express');
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on:http://localhost:${PORT}`);
+const handleHome = (req, res) => res.send("hello from server");
+const handleprofile = (req, res) => res.send("you are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("i'm between");
+  next();
+};
+
+//use middlewares
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+app.get("/profile", handleprofile);
+
+app.listen(PORT, handleListening);
