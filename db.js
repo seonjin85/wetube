d0f9a2b7f834f9a37c288a4971f522c307321012
SeonJin jang <seonjin85@gmail.com>
2020-07-07 22:08:44 +0900

    #3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,53 +1,17 @@
-export const videos = [
-  {
-    id: 32432,
-    title: "Vidoe awesome",
-    description: "This is something i love",
-    views: 24,
-    videoFile:
-    "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Seonjin",
-      email: "seonjin85@gmail.com",
-    },
-  },
-  {
-    id: 543223,
-    title: "Vidoe super",
-    description: "This is something i love",
-    views: 24,
-    videoFile:
-    "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Seonjin",
-      email: "seonjin85@gmail.com",
-    },
-  },
-  {
-    id: 4564,
-    title: "Vidoe nice",
-    description: "This is something i love",
-    views: 24,
-    videoFile:
-    "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Seonjin",
-      email: "seonjin85@gmail.com",
-    },
-  },
-  {
-    id: 8634,
-    title: "Vidoe perfect",
-    description: "This is something i love",
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Seonjin",
-      email: "seonjin85@gmail.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("☆Connected to DB");
+
+const handleError = (error) =>
+  console.log(`☆Error on DB Connection : ${error}`);
+
+db.once("open", handleOpen);
+db.once("error", handleError);
